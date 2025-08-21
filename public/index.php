93af3ada3e7b7fdaf2cd2e5de4ea9cc1bd8702a8
laxatzi <laxatzi@yahoo.com>
2025-08-21 13:06:51 +0300

    Display db  table in HTML format

--- public/index.php
+++ public/index.php
@@ -8,4 +8,26 @@ $pdo = new PDO($dsn, 'product_db_user', 'secret', [
 $stmt = $pdo->query("SELECT * FROM product");
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-print_r($products);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Products</title>
+
+</head>
+<body>
+  <h1>Products</h1>
+  <?php
+    foreach ($products as $product): ?>
+
+        <div class="product">
+        <h2><?= htmlspecialchars($product['name'])?></h2>
+        <p>Price:<?= htmlspecialchars($product['price'])?></p>
+
+        </div>
+    }
+  <?php endforeach; ?>
+</body>
+</html>
