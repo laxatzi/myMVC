82e5018b42230383896a6e416f55f712e8eac6bc
laxatzi <laxatzi@yahoo.com>
2025-08-21 12:36:49 +0300

    Getting some data from db and displaying them

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
-echo 'This is the index.php file in the public directory!!';
-// Serve existing files as-is (CSS/JS/images, etc.)
+
+$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
+$pdo = new PDO($dsn, 'product_db_user', 'secret', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+] );
+
+$stmt = $pdo->query("SELECT * FROM product");
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($products);
