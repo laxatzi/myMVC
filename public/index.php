e0385b66aae00abc3f846e66ff5f2cd9d9cc298a
laxatzi <laxatzi@yahoo.com>
2025-08-22 16:49:48 +0300

    Move logic from index to model.php and then require the file

--- public/index.php
+++ public/index.php
@@ -1,23 +1,6 @@
 <?php
 
-# Database connection
-
-$dsn = "mysql:host=localhost;dbname=product_db;charset=utf8;port=3306";
-
-// new PDO(...): Creates a PDO object (PHP Data Object), which is the modern way to connect to databases in PHP.
-// PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION: This makes sure that if something goes wrong, PHP will throw a clear error instead of silently failing.
-
-
-$pdo = new PDO($dsn, 'product_db_user', 'secret', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-] );
-
-// $pdo->query("SELECT * FROM product"): Sends a SQL query to MySQL.
-// $stmt: The result of the query (a statement object).
-// $stmt->fetchAll(PDO::FETCH_ASSOC): Fetches all the rows into a PHP array of associative arrays
-
-$stmt = $pdo->query("SELECT * FROM product");
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+require "model.php";
 
 ?>
 <!DOCTYPE html>
